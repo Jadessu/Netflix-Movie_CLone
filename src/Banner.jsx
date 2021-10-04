4a4d17c67c2808ef07cf00686ef552f52758aaf0
jadessu <junioradessu@gmail.com>
2021-10-04 18:28:00 -0500

    add linear gradient to banner

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -36,10 +36,12 @@ function Banner(){
                <button className="banner__button">Play</button>
                 <button className="banner__button">My List</button>
            </div>
-           <h1 className="banner__descriptions">{truncate(movie?.overview, 150)}</h1>
+           <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
            </div>
            
+       <div className="banner__fadeBottom">
 
+       </div>
        </header>
     )
 }
