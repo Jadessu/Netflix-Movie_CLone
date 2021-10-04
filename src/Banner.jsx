b34a502569453557950ba1b300391af50f8039e5
jadessu <junioradessu@gmail.com>
2021-10-04 17:56:00 -0500

    fetch 1 random movie

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -3,13 +3,13 @@ import axios from "./axios"
 import requests from "./request";
 
 function Banner(){
-    const [movies, setMovies] = useState([])
+    const [movie, setMovie] = useState([])
 
     useEffect(() => {
         async function fetchData(){
             const request = await axios.get(requests.fetchTrending)
 
-            setMovies(request.data.results[
+            setMovie(request.data.results[
                 Math.floor(Math.random() * request.data.results.length - 1 )
             ]
             )
@@ -18,7 +18,16 @@ function Banner(){
         fetchData()
     }, [])
     return (
-       <header>
+       <header className="banner"
+       style={{
+           backgroundSize: "cover",
+           backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+           backgroundPosition: "center center"
+       }}
+       >
+           <div className="banner__contents">
+            <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+           </div>
 
        </header>
     )
