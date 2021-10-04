76bc4f05fcc534a2a5e7e2829528d087f3985cad
jadessu <junioradessu@gmail.com>
2021-10-04 18:19:02 -0500

    create function to slice description

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Banner from './Banner';
 function App() {
   return (
     <div className="App">
+      <Banner/>
      
      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios"
 import requests from "./request";
+import "./Banner.css"
 
 function Banner(){
     const [movie, setMovie] = useState([])
@@ -17,6 +18,10 @@ function Banner(){
         }
         fetchData()
     }, [])
+
+    function truncate(str, n){
+        return str?.length > n ? str.substr(0, n-1) + "..." : str
+    }
     return (
        <header className="banner"
        style={{
@@ -26,8 +31,14 @@ function Banner(){
        }}
        >
            <div className="banner__contents">
-            <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+            <h1 className="banner__title">{movie?.title || movie?.name || movie?.original_name}</h1>
+           <div className="banner__buttons">
+               <button className="banner__button">Play</button>
+                <button className="banner__button">My List</button>
            </div>
+           <h1 className="banner__descriptions">{truncate(movie?.overview, 150)}</h1>
+           </div>
+           
 
        </header>
     )
