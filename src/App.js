daf684dead0772003f0d2daa522cff25f979400e
jadessu <junioradessu@gmail.com>
2021-10-28 20:46:44 -0500

    netlify fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import './App.css';
-import Row from './Row';
-import requests from './request';
-import Banner from './Banner';
-import Navbar from './Navbar';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Movies from './pages/Movies';
-import Dashboard from './pages/Dashboard';
 
 
 function App() {
